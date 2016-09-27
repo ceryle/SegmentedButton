5af6c73402fa5f8a5445ca66cf8d08cd89980b06
EGE <egeaker@gmail.com>
2016-09-27 17:22:28 +0300

    ...

--- library/src/main/java/co/ceryle/segmentedcontrol/RoundedCornerLayout.java
+++ library/src/main/java/co/ceryle/segmentedcontrol/RoundedCornerLayout.java
@@ -91,3 +91,113 @@ public class RoundedCornerLayout extends FrameLayout {
         cornerRadius = radius;
     }
 }
+
+/*
+    This layout first draws its contents to an offscreen bitmap,
+    masks the offscreen bitmap with a rounded rect and then draws the offscreen bitmap on the actual canvas.
+    I tried it and it seems to work (at least for my simple testcase).
+    It will of course affect performance compared to a regular layout.
+    http://stackoverflow.com/questions/26074784/how-to-make-a-view-in-android-with-rounded-corners
+*/
+
+/*
+        import android.content.Context;
+        import android.graphics.Bitmap;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Outline;
+        import android.graphics.Paint;
+        import android.graphics.PorterDuff;
+        import android.graphics.PorterDuffXfermode;
+        import android.graphics.RectF;
+        import android.os.Build;
+        import android.support.annotation.RequiresApi;
+        import android.util.AttributeSet;
+        import android.util.DisplayMetrics;
+        import android.util.TypedValue;
+        import android.view.View;
+        import android.view.ViewOutlineProvider;
+        import android.widget.FrameLayout;
+
+
+public class RoundedCornerLayout extends FrameLayout {
+    private float CORNER_RADIUS = 40.0f;
+
+    private Bitmap maskBitmap;
+    private Paint paint, maskPaint;
+    private float cornerRadius;
+
+    public void setRadius(float radius){
+        CORNER_RADIUS = radius;
+    }
+
+    public RoundedCornerLayout(Context context) {
+        super(context);
+        init(context, null, 0);
+    }
+
+    public RoundedCornerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public RoundedCornerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        cornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, CORNER_RADIUS, metrics);
+
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        maskPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+        maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+
+        setWillNotDraw(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setOutlineProvider(new ButtonOutlineProvider());
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Bitmap offscreenBitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas offscreenCanvas = new Canvas(offscreenBitmap);
+
+        super.draw(offscreenCanvas);
+
+        if (maskBitmap == null) {
+            maskBitmap = createMask(canvas.getWidth(), canvas.getHeight());
+        }
+
+        offscreenCanvas.drawBitmap(maskBitmap, 0f, 0f, maskPaint);
+        canvas.drawBitmap(offscreenBitmap, 0f, 0f, paint);
+    }
+
+    private Bitmap createMask(int width, int height) {
+        Bitmap mask = Bitmap.createBitmap(width, height, Bitmap.Config.ALPHA_8);
+        Canvas canvas = new Canvas(mask);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+
+        canvas.drawRect(0, 0, width, height, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        canvas.drawRoundRect(new RectF(0, 0, width, height), cornerRadius, cornerRadius, paint);
+
+        return mask;
+    }
+
+    //Fixes incorrect outline drawn by default
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private class ButtonOutlineProvider extends ViewOutlineProvider{
+        @Override
+        public void getOutline(View view, Outline outline) {
+            outline.setRoundRect(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight(), cornerRadius);
+        }
+    }
+}
+ */
